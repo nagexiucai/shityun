846213e3b0b94325b7a8969c4a2ceae874fb787e
xiucai <xiucai@pahuashan.cn>
2017-03-17 18:03:20 +0800

    reverse the order of clear in atexit

--- src/shityun.py
+++ src/shityun.py
@@ -6,9 +6,9 @@ import virt
 import web
 import atexit
 
-atexit.register(web.HTTPServ.clear)
-atexit.register(virt.Domain.clear)
 atexit.register(db.Model.clear)
+atexit.register(virt.Domain.clear)
+atexit.register(web.HTTPServ.clear)
 db.Model.initialize()
 virt.Domain.initialize()
 web.HTTPServ.initialize()
